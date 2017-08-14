6e90bc4983c55923f3b717a45be9b2b7754a6965
saqlz <saqlz@mail.ustc.edu.cn>
2017-08-14 21:09:06 +0800

    First commit

---
+++ PicWorkStation/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PicWorkStation
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            BitmapImage img = new BitmapImage (new Uri (@"C:\Users\qinglin.zhang\Desktop\timg.jpg"));
+            ImageCanvasControl.CanvasImageSource = img;
+        }
+
+
+        private void ImageCanvasControl_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //ImageCanvasControl.RenderTransform();
+        }
+
+        private void ImageCanvasControl_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("ImageCanvasControl_OnMouseLeftButtonUp");
+        }
+
+        private void ImageCanvasControl_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("ImageCanvasControl_OnMouseMove");
+        }
+        private void ImageCanvasControl_OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Point cen = e.GetPosition(ImageCanvasControl);
+            //(ImageCanvasControl.CanvasImageSource as Image).RenderTransform
+        }
+    }
+}
---
+++ PicWorkStation/ViewModels/ImageCanvas.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PicWorkStation
+{
+    public class ImageCanvas : Canvas
+    {
+        public ImageSource CanvasImageSource
+        {
+            get { return (ImageSource)GetValue(CanvasImageSourceProperty); }
+            set { SetValue(CanvasImageSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty CanvasImageSourceProperty =
+            DependencyProperty.Register("CanvasImageSource", typeof(ImageSource),
+            typeof(ImageCanvas), new FrameworkPropertyMetadata(default(ImageSource)));
+
+
+        private double _scale = 2.0;
+        public double Scale
+        {
+            get { return _scale; }
+            set
+            {
+                if (Math.Abs(value - _scale) < 1e-5 || value <= 1)
+                {
+                    return;
+                }
+                _scale = value;
+            }
+        }
+
+        private double _offsetX = 0.0;
+        public double OffsetX
+        {
+            get { return _offsetX; }
+            set
+            {
+                if (Math.Abs(value - _offsetX) < 1e-5)
+                {
+                    return;
+                }
+                _offsetX = value;
+            }
+        }
+
+        private double _offsetY = 0.0;
+        public double OffsetY
+        {
+            get { return _offsetY; }
+            set
+            {
+                if (Math.Abs(value - _offsetY) < 1e-5)
+                {
+                    return;
+                }
+                _offsetY = value;
+            }
+        }
+
+
+        protected override void OnRender(System.Windows.Media.DrawingContext dc)
+        {
+            dc.PushTransform(new TranslateTransform(_offsetX, _offsetY));
+            Rect rr = new Rect(0, 0, RenderSize.Width * _scale, RenderSize.Height * _scale);
+            dc.DrawImage(CanvasImageSource, rr);
+            base.OnRender(dc);
+        }
+
+    }
+}
