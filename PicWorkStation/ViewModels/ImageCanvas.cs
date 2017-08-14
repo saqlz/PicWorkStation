485f191414eca0a279f02f97049e82ce8137e73c
saqlz <saqlz@mail.ustc.edu.cn>
2017-08-15 01:55:39 +0800

    3

--- PicWorkStation/ViewModels/ImageCanvas.cs
+++ PicWorkStation/ViewModels/ImageCanvas.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace PicWorkStation
 {
+    [Serializable]
     public class ImageCanvas : Canvas
     {
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
         private ScaleTransform _scaleTransform;
         private TranslateTransform _translateTransform;
 
@@ -18,6 +26,37 @@ namespace PicWorkStation
         {
             _scaleTransform = new ScaleTransform(_scale, _scale);
             _translateTransform = new TranslateTransform(_offsetX, _offsetY);
+            this.ContextMenu = new ContextMenu();
+
+            var uploadImageMenu = new MenuItem();
+            uploadImageMenu.Header = "加载复制的图片";
+            uploadImageMenu.Click += btnLoadImage_Click;
+            this.ContextMenu.Items.Add(uploadImageMenu);
+
+            var uploadImageLinkMenu = new MenuItem();
+            uploadImageLinkMenu.Header = "加载复制链接地址的图片";
+            uploadImageLinkMenu.Click += btnLoadImageLink_Click;
+            this.ContextMenu.Items.Add(uploadImageLinkMenu);
+        }
+
+        private void btnLoadImageLink_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bmp = null;//new ClipboardMessage().GetBitmapSource();
+            if (bmp != null)
+            {
+                this.CanvasImageSource = bmp;
+                this.InvalidateVisual();
+            }
+        }
+
+        private void btnLoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            var bmp = new ClipboardMessage().ImageFromClipboardDib();
+            if (bmp != null)
+            {
+                this.CanvasImageSource = bmp;
+                this.InvalidateVisual();
+            }
         }
 
         public ImageSource CanvasImageSource
